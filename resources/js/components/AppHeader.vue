a640f8df4783807cf2035c223aee786391878dc2
Russell Jones <jonesrussell42@gmail.com>
2026-01-06 19:15:55 -0500

    Refactor AppLogo and AppLogoIcon components for improved styling and branding
    - Updated AppLogo component to use a new logo image and changed the displayed text to "StreetCode".
    - Simplified AppLogoIcon component by replacing the SVG with an image tag for better performance.
    - Adjusted AppHeader, AuthCardLayout, AuthSimpleLayout, and AuthSplitLayout to use the updated AppLogoIcon with consistent styling changes.

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -103,9 +103,7 @@ const rightNavItems: NavItem[] = [
                                 >Navigation Menu</SheetTitle
                             >
                             <SheetHeader class="flex justify-start text-left">
-                                <AppLogoIcon
-                                    class="size-6 fill-current text-black dark:text-white"
-                                />
+                                <AppLogoIcon class="size-6 dark:invert" />
                             </SheetHeader>
                             <div
                                 class="flex h-full flex-1 flex-col justify-between space-y-4 py-6"
